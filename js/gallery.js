0fe71500480b53d3835c05e96011bbcbc121cb87
bmtreuherz <bmtreuherz@ufl.edu>
2018-08-17 14:22:55 -0400

    Finished Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -2,80 +2,42 @@
 class Gallery extends React.Component {
 
   render() {
+    // Create Rows of Gallery Items
+    var projects = this.props.projects;
+    var rows = [];
+    for (let i=0; i<projects.length; i+=2){
+      var row = (
+        <div key={i} className="gallery_row">
+          <GalleryItem src={projects[i].pictures[0]}/>
 
-    var img_src = "img/" + this.props.projects[0].pictures[0];
+          {i+1 < projects.length ? (
+            <GalleryItem src={projects[i+1].pictures[0]}/>
+            ) : (
+              ''
+            )
+          }
+        </div>
+      );
+      rows.push(row);
+    }
 
+    // Return the gallery
     return (
       <div className="gallery_container">
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-
-        </div>
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/driftwood-sculpture.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-        </div>
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/driftwood-sculpture.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-        </div>
-
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-        </div>
-
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-        </div>
-
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/about_img.jpg"/>
-          </div>
-        </div>
-
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-          <div className="gallery_column">
-            <img src = "img/driftwood-sculpture.jpg"/>
-          </div>
-        </div>
-        <div className="gallery_row">
-          <div className="gallery_column">
-            <img src = "img/time-capsule.jpg"/>
-          </div>
-
-        </div>
+        {rows}
       </div>
     );
+  }
+}
+
+
+class GalleryItem extends React.Component{
+  render(){
+    return (
+      <div className="gallery_column">
+        <img src={"img/" + this.props.src}/>
+      </div>
+    )
   }
 }
 
@@ -94,8 +56,8 @@ function getProjectsList(){
       title: 'Project 2',
       description: 'Description 2',
       pictures: [
-        'time-capsule.jpg',
         'about_img.jpg',
+        'time-capsule.jpg',
         'driftwood-sculpture.jpg'
       ]
     },
@@ -103,9 +65,9 @@ function getProjectsList(){
       title: 'Project 3',
       description: 'Description 3',
       pictures: [
+        'driftwood-sculpture.jpg',
         'time-capsule.jpg',
-        'about_img.jpg',
-        'driftwood-sculpture.jpg'
+        'about_img.jpg'
       ]
     },
     {
@@ -121,9 +83,9 @@ function getProjectsList(){
       title: 'Project 5',
       description: 'Description 1',
       pictures: [
+        'driftwood-sculpture.jpg',
         'time-capsule.jpg',
-        'about_img.jpg',
-        'driftwood-sculpture.jpg'
+        'about_img.jpg'
       ]
     },
     {
@@ -137,6 +99,51 @@ function getProjectsList(){
     },
     {
       title: 'Project 7',
+      description: 'Description 1',
+      pictures: [
+        'about_img.jpg',
+        'time-capsule.jpg',
+        'driftwood-sculpture.jpg'
+      ]
+    },
+    {
+      title: 'New Project',
+      description: 'Description 1',
+      pictures: [
+        'time-capsule.jpg',
+        'about_img.jpg',
+        'driftwood-sculpture.jpg'
+      ]
+    },
+    {
+      title: 'Project 5',
+      description: 'Description 1',
+      pictures: [
+        'driftwood-sculpture.jpg',
+        'time-capsule.jpg',
+        'about_img.jpg'
+      ]
+    },
+    {
+      title: 'Project 6',
+      description: 'Description 1',
+      pictures: [
+        'time-capsule.jpg',
+        'about_img.jpg',
+        'driftwood-sculpture.jpg'
+      ]
+    },
+    {
+      title: 'Project 7',
+      description: 'Description 1',
+      pictures: [
+        'about_img.jpg',
+        'time-capsule.jpg',
+        'driftwood-sculpture.jpg'
+      ]
+    },
+    {
+      title: 'New Project',
       description: 'Description 1',
       pictures: [
         'time-capsule.jpg',
